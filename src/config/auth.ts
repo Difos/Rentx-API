ccf1a2f1f4b86863cf33e9a0dca53e7ac98abf03
Wesley E <difosx@gmail.com>
2021-11-12 08:48:47 -0300

    Delete auth.ts

--- src/config/auth.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    secret_token:"68cc20736358ce80d54199b144fc7eb8",
-    secret_refresh_token:"b8898b2938491be04647dbc52d77759c",
-    expires_in_token:"15m",
-    expires_in_refresh_token:"30d",
-    expires_refresh_token_days:30
-}
